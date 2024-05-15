cc15b8df80c52fad5a7d58acdb00516b87c71179
MrProvide <127773674+MrProvide@users.noreply.github.com>
2024-05-15 13:07:56 +0300

    first commit

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Java/Spring Boot/JavaScript these days. I've completed a programming course in Java and continue to explore the vast programming world. I also enjoy sports, primarily athletics like running and biking. Here is my <a href='https://github.com/MrProvide' target='_blank'>Github</a>!`,
+  "java-degree": `This is my Java degree. I'm proud to have achieved it as it provided me with a solid foundation in programming and a deep understanding of the language.`,
+  "sofa-table": `That's my sofa. I like to relax here, watch YouTube, and read my Daily.dev. Daily.dev is where I found this fascinating presentation of my portfolio you're currently exploring!`,
+  tv: `That's my TV. Lately, I've been watching a lot of tech YouTubers like <a href='https://www.youtube.com/@freecodecamp' target='_blank'>freeCodeCamp.org</a> and <a href='https://www.youtube.com/@MelkeyDev' target='_blank'>Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk, and on it is my resume. <a href='Resume' target='_blank'>Check it out?</a> Contact me at provideus123@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio: It's made with the Kaboom.js library, which is for making games in JavaScript. Text is rendered with HTML/CSS, so the textbox you're currently reading is not within the canvas. Learn more about how to use Kaboom.js by watching some tutorials <a href='https://youtube.com/@jslegenddev' target='_blank'>here</a>.`,
+  library: `I have a variety of books on my shelves, including some in Russian, English, and mostly Lithuanian.(I consider myself advanced in theses languages)
+   While I don't read as often as I'd like, becouse there's plenty of material to study online that can help improve my programming skills.`,
+  exit: `If you want to exit Rejus's portfolio, simply close the tab.`,
+};
